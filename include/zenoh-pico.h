e6719f9cdfacb44ee35cf2abf70176e786e637e5
Geoff Martin <geoff.martin@zettascale.tech>
2026-04-17 19:16:30 +0100

    Fixing formatting.

--- include/zenoh-pico.h
+++ include/zenoh-pico.h
@@ -21,7 +21,7 @@
 #define ZENOH_PICO_MAJOR 1
 #define ZENOH_PICO_MINOR 8
 #define ZENOH_PICO_PATCH 0
-#define ZENOH_PICO_TWEAK 
+#define ZENOH_PICO_TWEAK
 
 #include "zenoh-pico/api/admin_space.h"
 #include "zenoh-pico/api/advanced_publisher.h"
